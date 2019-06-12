c3e060c948c4b4c7b0b6cac29047b6b64e60f4c6
Thiago Fasano <Thiago@Fiotec.com>
2019-06-12 13:44:50 -0300

    backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 const app = express();
 
-const server = require('http').server(app);
+const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
 mongoose.connect('mongodb+srv://omni:omni123@cluster0-muldo.mongodb.net/test?retryWrites=true&w=majority', {
